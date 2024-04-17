51b4ccc74dad64b6fe00ad21e8a9c4da158d859f
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-04-16 21:27:26 -0600

    Exercise 1,2,3,4,5

---
+++ Modulo1/ejercicio6.js
@@ -0,0 +1,16 @@
+/**
+ * La tienda de drones de reparación está lista para abrir,
+ *  sólo falta escribir un script para ordenarlos. 
+ * Declarar variables y asignarles los valores adecuados:
+
+pricePerDroid - el precio de un dron, valor 800
+orderedQuantity - el número del pedido de los drones , valor 6
+deliveryFee - el costo de la entrega, valor 50
+totalPrice - el importe total del pedido a pagar, no se olvide de 
+los gastos de envío
+message - el mensaje del estado del pedido en el formato 
+"You ordered droids worth <total price> credits.
+ Delivery (<delivery fee> credits) is included in total price."
+ */
+
+ 
