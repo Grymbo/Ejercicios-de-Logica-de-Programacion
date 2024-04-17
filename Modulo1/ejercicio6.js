6e31e3255a78e679926fdeedc96d2cb84a539128
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-04-16 21:31:45 -0600

    exercise 7

--- Modulo1/ejercicio6.js
+++ Modulo1/ejercicio6.js
@@ -13,4 +13,9 @@ message - el mensaje del estado del pedido en el formato
  Delivery (<delivery fee> credits) is included in total price."
  */
 
- 
+ const pricePerDroid = 800;
+ const orderedQuantity = 6;
+ const deliveryFee = 50;
+ const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+ const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price`;
+  console.log(message);
